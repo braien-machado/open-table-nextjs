690a27c0c1c75b25e8319806a5bd4f2c412ae01d
Braien Machado de Paula <braienmp@outlook.com>
2023-07-29 20:23:28 -0300

    chore: nav and header

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,36 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className="text-2xl font-semibold">Open Table</h1>
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        {/* NAVBAR */}
+        <nav className="bg-white p-2 flex justify-between">
+          <a href="" className="font-bold text-gray-700 text-2xl">
+            OpenTable
+          </a>
+          <div>
+            <div className="flex">
+              <button type="button" className="bg-blue-400 text-white border px-4 py-1 rounded mr-3">
+                Sign in
+              </button>
+              <button type="button" className="border px-4 py-1 rounded">
+                Sign up
+              </button>
+            </div>
+          </div>
+        </nav>
+        {/* NAVBAR */}
+        <main>
+          {/* HEADER */}
+          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
+            <div className="text-center mt-10">
+              <h1 className="text-white text-2xl font-bold mb-2">
+                Find your table for any occasion
+              </h1>
+            </div>
+          </div>
+          {/* HEADER */}
+        </main>
+      </main>
     </main>
   )
 }
