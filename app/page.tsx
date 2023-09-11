f1c28ab45d770d13dedf6a37d468ba24417e7843
Braien Machado de Paula <braienmp@outlook.com>
2023-09-11 17:40:55 -0300

    refactor: create Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,16 @@
-"use client";
-
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 
 import NavBar from './components/NavBar';
+import Header from './components/Header';
 
 export default function Home() {
-  const router = useRouter();
-  const [location, setLocation] = useState("");
 
   return (
     <main className="bg-gray-100 min-h-screen w-full">
       <main className="max-w-screen-2xl m-auto bg-white">
         <NavBar />
         <main>
-          {/* HEADER */}
-          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
-            <div className="text-center mt-10">
-              <h1 className="text-white text-2xl font-bold mb-2">
-                Find your table for any occasion
-              </h1>
-              {/* SEARCHBAR */}
-              <div className="text-left py-3 m-auto flex justify-center">
-                <input
-                  type="text"
-                  className="rounded text-lg mr-3 p-2 w-[450px]"
-                  placeholder="State, city or town"
-                  onChange={(e) => setLocation(e.target.value)}
-                />
-                <button
-                  type="button"
-                  className="bg-red-600 px-9 py-2 text-white rounded"
-                  onClick={() => router.push(`/search/${location}`)}
-                >
-                  Let's go
-                </button>
-              </div>
-              {/* SEARCHBAR */}
-            </div>
-          </div>
-          {/* HEADER */}
+          <Header />
           {/* CARDS */}
           <div className="py-3 px-36 mt-10 flex flex-wrap">
             {/* CARD */}
