230f8133a4efe799bfbf4b02883c72b6ce540d75
Braien Machado de Paula <braienmp@outlook.com>
2023-09-11 17:43:45 -0300

    refactor: create RestaurantCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Link from 'next/link';
-
 import NavBar from './components/NavBar';
 import Header from './components/Header';
+import RestaurantCard from './components/RestaurantCard';
 
 export default function Home() {
 
@@ -11,44 +10,9 @@ export default function Home() {
         <NavBar />
         <main>
           <Header />
-          {/* CARDS */}
           <div className="py-3 px-36 mt-10 flex flex-wrap">
-            {/* CARD */}
-            <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-              <Link href="/restaurant/milestones-grill">
-                <img
-                  src="https://resizer.otstatic.com/v2/photos/legacy/3/25817615.jpg"
-                  alt=""
-                  className="w-full h-36"
-                  />
-                <div className="p-1">
-                  <h3 className="font-bold text-2xl mb-2">
-                    Milestones Grill
-                  </h3>
-                  <div className="flex items-start">
-                    <div className="flex mb-2">
-                      *****
-                    </div>
-                    <p className="ml-2">
-                      77 reviews
-                    </p>
-                  </div>
-                  <div className="flex text-base font-light capitalize">
-                    <p className="mr-3">
-                      Mexican
-                    </p>
-                    <p className="mr-3">$$$$</p>
-                    <p>Toronto</p>
-                  </div>
-                  <p className="text-sm mt-1 font-bold">
-                    Booked 3 times today
-                  </p>
-                </div>
-              </Link>
-            </div>
-            {/* CARD */}
+            <RestaurantCard />
           </div>
-          {/* CARDS */}
         </main>
       </main>
     </main>
