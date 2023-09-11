150cb689a6521792ed016b72defac7434d84345c
Braien Machado de Paula <braienmp@outlook.com>
2023-09-11 18:39:18 -0300

    refactor: create SearchBar component

--- app/search/components/Header.tsx
+++ app/search/components/Header.tsx
@@ -1,14 +1,9 @@
+import SearchBar from '../../components/SearchBar';
+
 export default function Header() {
   return (
     <div className="bg-gradient-to-r to-[#5f6984] from-[#0f1f47] p-2">
-      {/* SEARCHBAR */}
-      <div className="text-left py-3 m-auto flex justify-center">
-        <input type="text" className="rounded text-lg mr-3 p-2 w-[450px]" placeholder="State, city or town" />
-        <button type="button" className="bg-red-600 px-9 py-2 text-white rounded">
-          Let's go
-        </button>
-      </div>
-      {/* SEARCHBAR */}
+      <SearchBar />
     </div>
   )
 }
