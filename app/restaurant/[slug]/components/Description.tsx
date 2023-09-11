9919da675565734a39c1812d372e0944b0a4b9a1
Braien Machado de Paula <braienmp@outlook.com>
2023-09-11 18:03:41 -0300

    refactor: componentize Restaurant page

---
+++ app/restaurant/[slug]/components/Description.tsx
@@ -0,0 +1,15 @@
+export default function Description() {
+  return (
+    <div className="mt-4">
+      <p className="text-lg font-light">
+        Milestone Grill is open for indoor and outdoor dining
+      </p>
+      <p className="text-lg font-light">
+        Since 1977, Milestone Grill has been New York City's best steakhouse serving USDA Prime dry-aged steaks, premium seafood and award-winning wines.
+      </p>
+      <p className="text-lg font-light">
+        Located in Midtown Manhattan near Central Park, MOMA, Carnegie Hall, Rockefeller Center, Radio City Music Hall, and Times Square, Milestone Grill is the best steakhouse in Midtown Manhattan and New York City.
+      </p>
+    </div>
+  )
+}
---
+++ app/restaurant/[slug]/components/Rating.tsx
@@ -0,0 +1,17 @@
+export default function Rating() {
+  return (
+    <div className="flex items-end">
+      <div className="ratings mt-2 flex items-center">
+        <p>*****</p>
+        <p className="text-base ml-3">
+          4.9
+        </p>
+      </div>
+      <div>
+        <p className="text-base ml-4">
+          600 Reviews
+        </p>
+      </div>
+    </div>
+  )
+}
