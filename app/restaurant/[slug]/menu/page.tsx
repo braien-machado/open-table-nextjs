6479b592fefdcccf63663f1b056e17c47c709e40
Braien Machado de Paula <braienmp@outlook.com>
2023-09-11 18:13:06 -0300

    refactor: componentize RestaurantMenu page

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,55 +1,19 @@
-import Link from 'next/link';
 import NavBar from '../../../components/NavBar';
+import Header from '../components/Header';
+import Menu from '../components/Menu';
+import RestaurantNavBar from '../components/RestaurantNavBar';
 
-export default function Menu() {
+export default function RestaurantMenu() {
   return (
     <main className="max-w-screen-2xl m-auto bg-white">
       <NavBar />
-      {/* HEADER */}
-      <div className="h-96 overflow-hidden">
-        <div className="bg-center h-full flex justify-center items-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984]">
-          <h1 className="text-7xl text-white capitalize text-shadow text-center">
-            Milestones Grill (Toronto)
-          </h1>
-        </div>
-      </div>
-      {/* HEADER */}
-      {/* DESCRIPTION PORTION */}
+      <Header />
       <div className="flex m-auto w-2/3 justify-between items-start -mt-11">
         <div className="bg-white w-full rounded p-3 shadow">
-          {/* RESTAURANT NAVBAR */}
-          <nav className="flex text-base border-b pb-2">
-            <Link href="/restaurant/milestones-grill" className="mr-7">
-              Overview
-            </Link>
-            <Link href="/restaurant/milestones-grill/menu" className="mr-7">
-              Menu
-            </Link>
-          </nav>
-          {/* RESTAURANT NAVBAR */}
-          {/* MENU */}
-          <main className="bg-white mt-5">
-            <div>
-              <div className="mt-4 pb-1 mb-1">
-                <h1 className="font-bold text-4xl">Menu</h1>
-              </div>
-              <div className="flex flex-wrap justify-between">
-                {/* MENU CARD */}
-                <div className="border rounded p-3 w-[49%] mb-3">
-                  <h3 className="font-bold text-lg">Surf and Turf</h3>
-                  <p className="font-light mt-1 text-sm">
-                    A well done steak with lobster and rice
-                  </p>
-                  <p className="mt-7">$80</p>
-                </div>
-                {/* MENU CARD */}
-              </div>
-            </div>
-          </main>
-          {/* MENU */}
+          <RestaurantNavBar />
+          <Menu />
         </div>
       </div>
-      {/* DESCRIPTION PORTION */}
     </main>
   );
 }
